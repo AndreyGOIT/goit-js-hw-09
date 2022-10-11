0045829a4ea27cd6231dbb7241792c8216af6b41
AndreyGOIT <ae.borga@gmail.com>
2022-10-11 08:09:40 +0300

    another comment task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,7 @@
 // commonjs
-// const flatpickr = require('flatpickr');
+const flatpickr = require('flatpickr');
 // es modules are recommended, if available, especially for typescript
-import flatpickr from 'flatpickr';
+// import flatpickr from 'flatpickr';
 // Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
