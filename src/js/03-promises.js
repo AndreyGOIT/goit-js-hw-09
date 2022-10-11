31e4dc4308b3452277298468a79010e6bed0f829
AndreyGOIT <ae.borga@gmail.com>
2022-10-11 18:16:24 +0300

    task3 in progress

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,5 @@
 import Notiflix from 'notiflix';
+import throttle from 'lodash.throttle';
 // Напиши скрипт, который при сабмите формы вызывает функцию
 // createPromise(position, delay) столько раз, сколько ввели в поле amount.
 // При каждом вызове передай ей номер создаваемого промиса(position) и
@@ -7,51 +8,84 @@ const refs = {
   inputs: document.querySelectorAll('input'),
   submit: document.querySelector('button'),
 };
+// запись значений полей инпутов в локал сторадже
+const STORAGE_KEY = 'income-data';
+let formData = localStorage.getItem(STORAGE_KEY)
+  ? JSON.parse(localStorage.getItem(STORAGE_KEY))
+  : {};
 
-// const STORAGE_KEY = 'input-values';
-// let formData = localStorage.getItem(STORAGE_KEY)
-//   ? JSON.parse(localStorage.getItem(STORAGE_KEY))
-//   : {};
+refs.submit.addEventListener('submit', onFormSubmit);
+refs.submit.addEventListener(
+  'submit',
+  createPromise(refs.inputs[2].value, refs.inputs[0].value)
+);
 
-// const settings = {
-//   firstDelay: refs.inputs[0].value,
-//   delayStep: 'refs.inputs[1].value',
-//   amount: 'refs.inputs[2].value',
-// };
-// console.log(refs.inputs[0]);
-// localStorage.setItem('settings', JSON.stringify(settings));
+refs.inputs[0].addEventListener('input', throttle(onTextInput, 500));
+refs.inputs[1].addEventListener('input', throttle(onTextInput, 500));
+refs.inputs[2].addEventListener('input', throttle(onTextInput, 500));
 
-// refs.inputs[0].addEventListener('input', onTextInput());
+populateTextarea();
 
-// function onTextInput(event) {
-//   // console.log((formData[event.target.name] = event.target.value));
-//   formData[event.target.name] = event.target.value;
-//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-// }
-// let delay = firstDelay;
-// function onClickSubmit() {
-//   console.log('Нажата кнопка Create promises');
-//   for (let i = 1; i <= amount; i++) {
-//     const position = i;
-//     console.log(`Position value is ${position}`);
-//     createPromise(position, delay);
-//     delay += delayStep;
-//     console.log(`Delay value is ${delay}`);
-//   }
-// }
+function onTextInput(event) {
+  // console.log((formData[event.target.name] = event.target.value));
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
 
-refs.submit.addEventListener('submit', onSubmitForm());
+// 2. При загрузке страницы проверяй состояние хранилища, и если там есть сохраненные
+// данные, заполняй ими поля формы.В противном случае поля должны быть пустыми.
+function populateTextarea() {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  console.log(savedMessage);
+  if (savedMessage) {
+    refs.inputs[0].value = savedMessage.delay || '';
+    refs.inputs[1].value = savedMessage.step || '';
+    refs.inputs[2].value = savedMessage.amount || '';
+  }
+}
 
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   } else {
-//     // Reject
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   }
-// }
+// 3. При сабмите формы очищай хранилище и поля формы, а также выводи объект с
+// полями delay, step, amount  и текущими их значениями в консоль.
+function onFormSubmit(event) {
+  console.log('сработала функция onFormSubmit');
+  event.preventDefault();
+
+  if (refs.inputs[0].value && refs.inputs[1].value && refs.inputs[2].value) {
+    console.log('Отправляем форму');
+    event.currentTarget.reset();
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    localStorage.removeItem(STORAGE_KEY);
+    formData = {};
+  }
+}
+
+function createPromise(position, delay) {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  let actualDelay = savedMessage.delay;
+  console.log(actualDelay);
+  for (let position = 1; position <= savedMessage.amount; position += 1) {
+    const promis = new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
+      setTimeout(() => {
+        if (shouldResolve) {
+          // Fulfill
+          console.log('создаем промис успешено');
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${actualDelay}ms`
+          );
+        } else {
+          // Reject
+          console.log('создаем промис неуспешно');
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${actualDelay}ms`
+          );
+        }
+      }, actualDelay);
+      actualDelay += savedMessage.step;
+    });
+  }
+}
+
 // Дополни код функции createPromise так, чтобы она возвращала один промис,
 // который выполянется или отклоняется через delay времени.Значением промиса
 // должен быть объект, в котором будут свойства position и delay со значениями
@@ -64,28 +98,3 @@ refs.submit.addEventListener('submit', onSubmitForm());
 //   .catch(({ position, delay }) => {
 //     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
 //   });
-
-function onSubmitForm(evt) {
-  // evt.preventDefault();
-  let delay = Number(evt.target.delay.value);
-  let amount = Number(evt.target.amount.value);
-  let step = Number(evt.target.step.value);
-
-  for (let position = 1; position <= amount; position += 1) {
-    setTimeout(() => {
-      createPromise(position, delay)
-        .then(({ position, delay }) => {
-          Notiflix.Notify.success(
-            `✅ Fulfilled promise ${position} in ${delay}ms`
-          );
-        })
-        .catch(({ position, delay }) => {
-          Notiflix.Notify.failure(
-            `❌ Rejected promise ${position} in ${delay}ms`
-          );
-        });
-    }, delay);
-    delay += step;
-    formats.reset();
-  }
-}
